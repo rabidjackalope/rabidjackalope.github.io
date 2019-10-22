f99668254f6fa99cfa32b3ecebe8b366291b2b75
DavidBarney <rabidjackalope@gmail.com>
2019-10-21 22:30:29 -0700

    created lesson6 folder

---
+++ lesson6/js/reminder.js
@@ -0,0 +1,1 @@
+new Date().getDay() != 5 ? document.getElementById('reminder').innerHTML =  banner.style.display = "none" : "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion.";
