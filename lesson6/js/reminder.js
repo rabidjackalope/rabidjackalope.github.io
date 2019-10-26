7a8c32959247142617bb7ca21d36d64a45e292f7
DavidBarney <rabidjackalope@gmail.com>
2019-10-25 21:00:06 -0700

    reminder.js update

--- lesson6/js/reminder.js
+++ lesson6/js/reminder.js
@@ -1,1 +1,1 @@
-new Date().getDay() != 5 ? document.getElementById('reminder').innerHTML =  banner.style.display = "none" : "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion.";
+new Date().getDay() == 5 ? document.getElementById('reminder').innerHTML = "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion." : banner.style.display = "none";
