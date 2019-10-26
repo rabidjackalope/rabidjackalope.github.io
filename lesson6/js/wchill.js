920b8bd93a0751238d8fbedc9257011b543a5ec7
DavidBarney <rabidjackalope@gmail.com>
2019-10-26 13:10:49 -0700

    syntax updates

--- lesson6/js/wchill.js
+++ lesson6/js/wchill.js
@@ -2,7 +2,7 @@ var tempF = parseFloat(document.getElementById("temp").textContent);
 var velo = parseFloat(document.getElementById("speed").textContent);
 
 if (isNaN(tempF) || isNaN(velo)) {
-    document.getElementById("chill").innerHTML = "N/A"
+    document.getElementById("chill").textContent = "N/A"
 } else {
     document.getElementById("chill").innerHTML = calc(tempF, velo).toFixed(0) + " &#8457;";
 }
