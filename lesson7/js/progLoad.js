12d2de966140316c4b541ef6f242572cad215f62
DavidBarney <rabidjackalope@gmail.com>
2019-10-31 19:55:43 -0700

    edits

--- lesson7/js/progLoad.js
+++ lesson7/js/progLoad.js
@@ -1,26 +1,26 @@
 let imagesToLoad = document.querySelectorAll('img[data-src]');
-const loadImages = (image) => {
-  image.setAttribute('src', image.getAttribute('data-src'));
-  image.onload = () => {
-    image.removeAttribute('data-src');
-  };
-};
- 
 
-if('IntersectionObserver' in window) {
+if ('IntersectionObserver' in window) {
     const observer = new IntersectionObserver((items, observer) => {
-      items.forEach((item) => {
-        if(item.isIntersecting) {
-          loadImages(item.target);
-          observer.unobserve(item.target);
-        }
-      });
+        items.forEach((item) => {
+            if (item.isIntersecting) {
+                loadImages(item.target);
+                observer.unobserve(item.target);
+            }
+        });
     });
     imagesToLoad.forEach((img) => {
-      observer.observe(img);
+        observer.observe(img);
     });
-  } else {
+} else {
     imagesToLoad.forEach((img) => {
-      loadImages(img);
+        loadImages(img);
     });
-  }
+}
+
+const loadImages = (image) => {
+    image.setAttribute('src', image.getAttribute('data-src'));
+    image.onload = () => {
+        image.removeAttribute('data-src');
+    };
+};
